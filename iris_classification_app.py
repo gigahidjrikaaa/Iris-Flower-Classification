1738088da2b58aed621147d6b95f6d70605def5d
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2025-02-19 20:32:32 +0700

    feat: changed my mind to not use scikit-learn

--- iris_classification_app.py
+++ iris_classification_app.py
@@ -5,9 +5,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 # Load the Iris dataset
 iris = load_iris()
